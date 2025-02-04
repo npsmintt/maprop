57c186daad3fa8d216088badd5cffd82c380216e
Napasorn Jutrakul <145433745+npsmintt@users.noreply.github.com>
2025-02-04 19:41:56 +0700

    add marker to map

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,54 +1,81 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSliders } from "@fortawesome/free-solid-svg-icons";
-import { faTag } from "@fortawesome/free-solid-svg-icons";
 import "@fontsource/noto-sans-thai/800.css";
 import "@fontsource/noto-sans-thai/600.css";
 import "@fontsource/noto-sans-thai/400.css";
 import "../css/index.css";
-import { APIProvider, Map } from "@vis.gl/react-google-maps";
+import { APIProvider, Map, AdvancedMarker, InfoWindow } from "@vis.gl/react-google-maps";
 import ListingList from "./listingList";
+import { createClient } from "@supabase/supabase-js";
 
 const googleAPI = process.env.REACT_APP_GOOGLE_MAP_API_KEY;
+const supabase = createClient(
+  process.env.REACT_APP_SUPABASE_URL,
+  process.env.REACT_APP_SUPABASE_ANON_KEY
+);
 
 const Home = () => {
   const filterIcon = <FontAwesomeIcon icon={faSliders} size="md" />;
   const [keyword, setKeyword] = useState("");
+  const [listings, setListings] = useState([]);
+  const [hoveredListing, setHoveredListing] = useState(null);
+  const [selectedListing, setSelectedListing] = useState(null);
   // const [filter, setFilter] = useState("");
+
+  useEffect(() => {
+    fetchListings();
+  }, []);
+
+  async function fetchListings () {
+    const { data, error } = await supabase
+      .from("listings")
+      .select("listing_id, name, lat, lng, tradetype");
+
+    if (error) {
+      console.error("Error fetching listings: ", error.message);
+      return;
+    }
+
+    setListings(data);
+  };
+
   const handleSearch = (event) => {
     event.preventDefault();
     alert(`You searched for '${keyword}'`);
     // function to search property
   };
+
   const handleChange = (event) => {
     setKeyword(event.target.value);
   };
+
   const handleFilter = (event) => {
     setKeyword(event.target.value);
   };
 
   return (
     <>
-      <div className="searchArea">
+      <div className="search">
         <form onSubmit={handleSearch}>
           <input
-            className="searchInput"
+            className="search__input"
             name="keyword"
             placeholder="ค้นหาด้วยคีย์เวิร์ด"
             value={keyword}
             onChange={handleChange}
           />
-          <button type="submit" className="searchButton">
+          <button type="submit" className="search__btn">
             ค้นหา
           </button>
         </form>
-        <button type="button" className="filterButton" onClick={handleFilter}>
+        <button type="button" className="search__filter" onClick={handleFilter}>
           {filterIcon}
           <p>ตัวกรอง</p>
         </button>
       </div>
       <div className="container">
-        <div className="map">
+        <div className="container__map">
           <APIProvider
             apiKey={googleAPI}
             onLoad={() => console.log("Maps API has loaded.")}
@@ -60,37 +87,34 @@ const Home = () => {
                 lat: 13.705242300007146,
                 lng: 100.63543752886405,
               }}
-            />
+              onClick={() => {
+                setSelectedListing(null);
+              }}
+            >
+              {listings.map((listing) => (
+                <React.Fragment key={listing.listing_id}>
+                  <AdvancedMarker
+                    key={listing.listing_id}
+                    position={{ lat: listing.lat, lng: listing.lng }}
+                    onMouseEnter={() => setHoveredListing(listing.listing_id)}
+                    onMouseLeave={() => setHoveredListing(null)}
+                    onClick={() => setSelectedListing(listing.listing_id)}
+                  >
+                    <div className={`custom__marker ${listing.tradetype === "เช่า" ? "marker--rent" : "marker--sell"}`}>
+                  {hoveredListing === listing.listing_id && (
+                    <div className="marker--label">{listing.name}</div>
+                  )}
+                  </div>
+                  </AdvancedMarker>
+                </React.Fragment>
+              ))}
+            </Map>
           </APIProvider>
         </div>
-        <div className="listHolder">
-          <div className="listHolderHeader">
-            <div>
-              <p>
-                รายชื่อรายการ
-                <br />
-                (100 รายการ)
-              </p>
-            </div>
-            <div className="selectContainer">
-              <label htmlFor="sorting">เรียงจาก</label>
-              <div className="select-wrapper">
-                <select name="sorting" id="sorting" defaultValue="pricelth">
-                  <option value="pricelth">
-                    ราคา (น้อย-มาก)
-                  </option>
-                  <option value="pricehtl">ราคา (มาก-น้อย)</option>
-                  <option value="areastb">พื้นที่ (เล็ก-ใหญ่)</option>
-                  <option value="areabts">พื้นที่ (ใหญ่-เล็ก)</option>
-                </select>
-              </div>
-            </div>
-          </div>
-          <div className="listingContainer">
-            <ListingList />
+          <div className="container__listing">
+            <ListingList selectedListing={selectedListing}/>
           </div>
         </div>
-      </div>
     </>
   );
 };
--- src/pages/listingList.jsx
+++ src/pages/listingList.jsx
@@ -2,16 +2,17 @@ import { useEffect, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTag } from "@fortawesome/free-solid-svg-icons";
 import { createClient } from "@supabase/supabase-js";
+import "../css/listingList.css";
 
-// Initialize Supabase client
 const supabase = createClient(
-  process.env.REACT_APP_SUPABASE_URL,
-  process.env.REACT_APP_SUPABASE_ANON_KEY
-);
+    process.env.REACT_APP_SUPABASE_URL,
+    process.env.REACT_APP_SUPABASE_ANON_KEY
+  );
 
-const ListingList = () => {
+const ListingList = ({ selectedListing }) => {
     const priceIcon = <FontAwesomeIcon icon={faTag} size="sm" />;
     const [listing, setListing] = useState([]);
+    const [totalListing, setTotalListin] = useState(0);
 
     useEffect(() => {
         console.log("Fetching listings...");
@@ -19,36 +20,82 @@ const ListingList = () => {
     }, []);
 
     async function getListing() {
-        const { data, error } = await supabase.from("listings").select();
+        const { data, error } = await supabase
+            .from("listings")
+            .select(`
+                listing_id,
+                name,
+                price,
+                bedroom,
+                bathroom,
+                tradetype,
+                status,
+                listingTypes:type (name)`)
+            .order("name", {ascending: false});
 
         if (error) {
             console.error("Error fetching listings:", error.message);
             return;
         }
 
+        const { count, error: countError } = await supabase
+            .from("listings")
+            .select("*", {count: "exact", head: true});
+        
+        if (countError) {
+            console.error("Error fetching listing count: ", countError.message);
+            return;
+        }
+
         setListing(data);
+        setTotalListin(count);
     }
+
+    const formatPrice = (price) => {
+        return new Intl.NumberFormat('en-US', {
+            minimumFractionDigits: 2,
+            maximumFractionDigits: 2,
+        }).format(price);
+    };
 
     return (
         <>
-        <div className="listingPic">photo</div>
-
-            <ul>
-                {listing.map((item) => (
-                    <li key={item.id}>{item.name}</li>
-                ))}
-            </ul>
-
-            <div className="listing">
-              <div className="Tagging">
-                <p className="statusTag">สถานะ</p>
-                <p className="salesTypeTag">ขาย/เช่า</p>
-              </div>
-              <p className="listingName">ชื่อรายการ</p>
-              <p className="listingID">ID: ?</p>
-              <p className="listingType">ประเภท | ห้องนอน ? ห้องน้ำ ?</p>
-              <p className="listingPrice">{priceIcon} ? บาท</p>
+        <div className="listbox">
+          <div className="listbox__header">
+            <div>
+              <p className="header--bold">รายชื่อรายการ</p>
+              <p>({totalListing} รายการ)</p>
             </div>
+            <div className="header--select">
+              <label htmlFor="sorting">เรียงจาก</label>
+              <div className="select-wrapper">
+                <select name="sorting" id="select--sorting" defaultValue="pricelth">
+                  <option value="pricelth">
+                    ราคา (น้อย-มาก)
+                  </option>
+                  <option value="pricehtl">ราคา (มาก-น้อย)</option>
+                  <option value="areastb">พื้นที่ (เล็ก-ใหญ่)</option>
+                  <option value="areabts">พื้นที่ (ใหญ่-เล็ก)</option>
+                </select>
+              </div>
+            </div>
+          </div>
+        {listing.map((item) => (
+            <div key={item.listing_id} className={`listbox__listing ${selectedListing === item.listing_id ? "listing--highlight" : ""}`}>
+                <div className="listing--pic">photo</div>
+                    <div className="listing">
+                        <div className="desc--tagging">
+                            <p className={`tagging--status ${item.status === "Active" ? "status--active" : "status--inactive"}`}>{item.status}</p>
+                            <p className={`tagging--trade ${item.tradetype === "เช่า" ? "trade--renting" : "trade--selling"}`}>{item.tradetype}</p>
+                        </div>
+                    <p className="desc--name">{item.name}</p>
+                    <p className="desc--id">ID: {item.listing_id}</p>
+                    <p className="desc--type">{item.listingTypes?.name} | {item.bedroom ? item.bedroom : 0} ห้องนอน {item.bathroom ? item.bathroom : 0} ห้องน้ำ</p>
+                    <p className="desc--price">{priceIcon} {formatPrice(item.price)} บาท</p>
+                    </div>
+            </div>
+        ))}
+        </div>
         </>
     );
 };
